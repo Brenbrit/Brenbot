6990baea596a35c47bc87bddd69b1a081ccb390b
Brenbrit <brendanguillory@gmail.com>
2019-07-19 22:35:48 -0500

    Actually made the bot do something

--- bot/bot.py
+++ bot/bot.py
@@ -1,3 +1,17 @@
-print("hi")
-f = open("testmake.txt", "w")
-f.close()
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+client.run("NDU3Mjg1MDE0NjMyMjY3Nzg5.XTKLSg.WcbluBpyBaJBPUF8PKy4G5gX5hs")
