6ff8156c94b56cf95dffd51e60e1967100d2617d
Brenbrit <brendanguillory@gmail.com>
2019-07-19 23:06:33 -0500

    Update bot.py

--- bot/bot.py
+++ bot/bot.py
@@ -1,7 +1,22 @@
 import discord
+import os
+import platform
+import subprocess
 
 #used in the @ parts and at the client.run line
 client = discord.Client()
+currentPlatform = platform.system().lower()
+
+def getToken():
+    
+    if currentPlatform.startswith("linux"):
+        loc = "/home/ubuntu/Brenbot/bot/codes.txt"
+    elif currentPlatform.startswith("win"):
+        loc = "C:\\Users\\Brenbrit\\Documents\\Brenbot\\bot\\codes.txt"
+    file = open(loc, "r")
+    return file.readline().split(":")[1]
+    
+        
 
 @client.event
 async def on_ready():
@@ -14,7 +29,16 @@ async def on_message(message):
 
     if message.content.lower().startswith("update"):
         await message.channel.send('ok')
+        update()
 
 
 #this actually starts the bot
-client.run("NDU3Mjg1MDE0NjMyMjY3Nzg5.XTKLSg.WcbluBpyBaJBPUF8PKy4G5gX5hs")
+token = getToken()
+client.run(token)
+
+
+def update():
+    if currentPlatform.startswith("linux"):
+        print("gotem")
+        subprocess.run(["sh /home/ubuntu/update.sh"])
+    
