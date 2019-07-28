6fa78e5862f140784ad3fa9a44a5b98515cde801
Brenbrit <brendanguillory@gmail.com>
2019-07-28 17:45:34 -0500

    soft tabs

--- bot/processors.py
+++ bot/processors.py
@@ -1,11 +1,11 @@
 from utils import update
 
 def process(message):
-	out = [[],[]]
-	print("this is where the processing will happen")
+    out = [[],[]]
+    print("this is where the processing will happen")
 
-	if message.content.lower().startswith("update"):
+    if message.content.lower().startswith("update"):
         out[0].append([message.channel, "ok"])
         update()
 
-	return [[],[]]
+    return [[],[]]
