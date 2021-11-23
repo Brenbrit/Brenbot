8f82b92c24cef7d77ac3634c58c6cfdac545f9fe
Brendan Guillory <brendanguillory@gmail.com>
2021-11-22 20:37:20 -0600

    Updated run.sh to remove Britbot's old token. Oops!

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 echo "Packaging"
 mvn package -q
 echo "Executing"
-mvn exec:java -q -Dexec.mainClass=com.brenbrit.brenbot.Bot -Dexec.classpathScope=runtime -Dexec.args=ODAxNDk0NjQ2NjEzNzM3NTAy.YAhgDg.NQb3GFTZRrJ2Q_s0ZjbTOHL0DNY
+mvn exec:java -q -Dexec.mainClass=com.brenbrit.brenbot.Bot -Dexec.classpathScope=runtime -Dexec.args=$(echo /usr/share/brenbot/token)
