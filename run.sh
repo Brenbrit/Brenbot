f017d11b4902918cbb912a904cc70efa6fe4ab49
Brendan Guillory <brendanguillory@gmail.com>
2021-11-28 17:18:52 -0600

    Made run.sh more verbose

--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 # How many arguments are there?
 if [ $# -gt 0 ]
 then
+		echo "Using Maven located at $1."
 		mvnloc=$1
 else
+		echo "Using Maven located at $(whereis mvn | cut -f 2 -d ' ')."
 		mvnloc=$(whereis mvn | cut -f 2 -d ' ')
 fi
 
