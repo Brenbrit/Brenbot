b4608dbb5826a0aa376f2423e5738a472cb43718
Brendan Guillory <brendanguillory@gmail.com>
2021-11-28 16:29:21 -0600

    Updated Bot.java

--- src/main/java/com/brenbrit/brenbot/Bot.java
+++ src/main/java/com/brenbrit/brenbot/Bot.java
@@ -1,27 +1,73 @@
 package com.brenbrit.brenbot;
 
 import discord4j.core.DiscordClient;
+import discord4j.core.event.domain.lifecycle.ReadyEvent;
 import discord4j.core.GatewayDiscordClient;
-import discord4j.core.object.entity.Message;
-import discord4j.core.event.domain.message.MessageCreateEvent;
+//import discord4j.core.object.entity.Message;
+//import discord4j.core.event.domain.message.MessageCreateEvent;
+import discord4j.core.object.entity.User;
+import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Properties;
 
 public class Bot {
 
-    public static void main(String[] args) {
-        System.out.println("Starting up Brenbot.");
+    public static final String PROPERTIES_LOC = "config.properties";
+    private Properties properties;
 
-        final String token = args[0];
+    public static void main(String[] args) {
+        new Bot().start();
+    }
+
+    public Bot() {
+        this.properties = readProperties(Bot.PROPERTIES_LOC);
+    }
+
+    public void start() {
+
+        System.out.println("Starting up Brenbot.");
 
         // A DiscordClient only represents the operations we can do while not
         // logged in. To do other bot things, we've got to log in.
-        final DiscordClient client = DiscordClient.create(token);
-        final GatewayDiscordClient gateway = client.login().block();
+        final DiscordClient client = DiscordClient.create(properties.getProperty("discord.token"));
+        Mono<Void> login = client.withGateway((GatewayDiscordClient gateway) ->
+            gateway.on(ReadyEvent.class, event ->
+                Mono.fromRunnable(() -> {
+                    final User self = event.getSelf();
+                    System.out.printf("Logged in as %s#%s%n", self.getUsername(), self.getDiscriminator());
+                })));
 
-        gateway.on(MessageCreateEvent.class).subscribe(event -> {
-            final Message message = event.getMessage();
-            if ("hello".equals(message.getContent().toLowerCase()))
-                message.getChannel().block().
-                    createMessage("world! Programmed to work and not to feel!").block();
-        });
+        System.out.println("Blocking");
+        login.block();
+    }
+
+    public static Properties readProperties(String fileName) {
+        FileInputStream fis = null;
+        Properties prop = null;
+
+        try {
+            fis = new FileInputStream(fileName);
+            prop = new Properties();
+            prop.load(fis);
+        } catch (FileNotFoundException fnfe) {
+            System.out.println("Propperties file not found at " + fileName + ".");
+            fnfe.printStackTrace();
+            System.exit(1);
+        } catch (IOException ioe) {
+            System.out.println("IOException while readidng Properties file at " + fileName + ".");
+            ioe.printStackTrace();
+            System.exit(1);
+        } finally {
+            try {
+                fis.close();
+            } catch (IOException ioe) {
+                System.out.println("IOException while closing properties file.");
+                ioe.printStackTrace();
+            }
+        }
+        return prop;
     }
 }
