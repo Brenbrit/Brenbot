7876f07a213a5e50d6f39e13d2eeadb329a2024e
Brendan Guillory <brendanguillory@gmail.com>
2021-12-21 06:07:26 -0600

    Fixed silly mistake: forgetting /bin

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ then
 echo "Using Maven located at $1."
 mvnloc=$1
 export JAVA_HOME="/etc/jdk-16.0.2+7"
-export PATH="/etc/jdk-16.0.2+7:$PATH"
+export PATH="/etc/jdk-16.0.2+7/bin:$PATH"
 else
 echo "Using Maven located at $(type -p mvn)."
 mvnloc=$(type -p mvn)
