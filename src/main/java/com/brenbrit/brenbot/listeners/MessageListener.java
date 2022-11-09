1cbcf70259a14e300c6ae6067bafb3e33b283f59
Brendan Guillory <brendanguillory@gmail.com>
2022-11-08 18:47:31 -0600

    added pip's request

--- src/main/java/com/brenbrit/brenbot/listeners/MessageListener.java
+++ src/main/java/com/brenbrit/brenbot/listeners/MessageListener.java
@@ -49,6 +49,13 @@ public class MessageListener extends ListenerAdapter {
             }
         }
 
+        // Is the message from profawnsor?
+        //if (msg.getAuthor().getId() == 341282409565650964) {
+        if (msg.getAuthor().getId() == 316021839220572160) {
+            logger.info("Profawnsor found");
+            msg.getTextChannel().sendMessage("https://media.discordapp.net/attachments/676920706243493898/1039347070915579974/togif.gif").queue();
+        }
+
         // Check and fix embeds
         embedFixer.checkAndFixEmbeds(msg);
     }
